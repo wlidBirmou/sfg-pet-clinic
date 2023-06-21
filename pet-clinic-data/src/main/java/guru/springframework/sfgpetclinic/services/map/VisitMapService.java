6e353fac965e987a38c2751d4a3b19b0d5d5bb46
wlidBirmou <laakab@gmail.com>
2023-06-20 22:47:30 -0400

    Edit of Save method on Visit map service.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -18,8 +18,10 @@ public class VisitMapService extends AbstractMapService<Visit,Long>  implements 
     }
 
     @Override
-    public Visit save(Visit object) {
-        return super.save(object);
+    public Visit save(Visit visit) {
+        if(visit.getPet()==null ||visit.getPet().getOwner()==null || visit.getPet().getId()==null || visit.getPet().getOwner().getId()==null)
+            throw new RuntimeException("Invalid visit");
+        return super.save(visit);
     }
 
     @Override
