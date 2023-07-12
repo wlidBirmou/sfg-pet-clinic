f4982d96faac894c42cd2228f13a4dabab10387b
wlidBirmou <laakab@gmail.com>
2023-07-12 12:13:19 -0400

    Creation of ownerDetails.html template add of findOwner method in OwnerController + tests, closes #49

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,10 +1,14 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/owners")
@@ -17,7 +21,7 @@ public class OwnerController {
     }
 
 
-    @RequestMapping({"","/","/index","/index.html"})
+    @GetMapping({"","/","/index","/index.html"})
     public String listOwners(Model model){
 
         model.addAttribute("owners",this.ownerService.findAll());
@@ -25,10 +29,17 @@ public class OwnerController {
         return "owners/index";
     }
 
-    @RequestMapping("/find")
+    @GetMapping("/find")
     public String findOwners(){
         return "notimplemented";
     }
 
+    @GetMapping("/{id}")
+    public ModelAndView findOwner(@PathVariable Long id){
+        ModelAndView modelAndView=new ModelAndView("owners/ownerDetails");
+        Owner owner=this.ownerService.findById(id);
+        modelAndView.addObject("owner",owner);
+        return  modelAndView;
+    }
 
 }
