a30a6268f58ede6ce601ca1727386d9f79a09aeb
wlidBirmou <laakab@gmail.com>
2023-07-12 12:56:40 -0400

    Correction of PetType entity (remove of Pet relationship, add of toString method) Correction in ownerDetails.html template (add of petType.toString())

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -17,13 +17,14 @@ public class PetType  extends BaseEntity{
     @Column(name="name")
     private String name;
 
-    @OneToMany(mappedBy = "petType", cascade = CascadeType.ALL )
-    Set<Pet> pets=new LinkedHashSet<>();
-
-
     @Builder
     public PetType(Long id, String name) {
         super(id);
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.getName();
     }
 }
